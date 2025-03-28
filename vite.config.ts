44b6a67a7b01651cbc8d8a55c8d5838ed9466d7b
Vedant <vedantp3@gmail.com>
2025-03-29 00:29:47 +0530

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext'  // Allows top-level await
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
