a10122e7e254f3f46c3f35ba5a41c24f0dbd992f
Vedant <vedantp3@gmail.com>
2025-04-04 19:30:01 +0530

    added netlify.toml

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-// import './assets/timeburner-font/Timeburner-xJB8.ttf'
-// import './assets/timeburner-font/TimeburnerBold-peGR.ttf'
 
 
 createRoot(document.getElementById('root')!).render(
