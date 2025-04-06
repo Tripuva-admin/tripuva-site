d3753c3d1532f412fdcce8fcbe3ecfa0d5ce4846
Vedant <vedantp3@gmail.com>
2025-04-06 18:14:20 +0530

    updating start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,11 @@
 ENV=${1:-dev}
 
 # URL of your Supabase bucket (public bucket)
-BASE_URL="https://oahorqgkqbcslflkqhiv.supabase.co/storage/v1/object/sign/env-files/dev/.env.dev?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJlbnYtZmlsZXMvZGV2Ly5lbnYuZGV2IiwiaWF0IjoxNzQzOTQyOTI4LCJleHAiOjE3NzU0Nzg5Mjh9.fTpcaD-6x6BDnzdWlr4PkJ1qwSSTRsi25aJ9R00mKiw"
+BASE_URL="https://oahorqgkqbcslflkqhiv.supabase.co/storage/v1/object/sign/env-files"
+TOKEN="token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJlbnYtZmlsZXMvZGV2Ly5lbnYuZGV2IiwiaWF0IjoxNzQzOTQyOTI4LCJleHAiOjE3NzU0Nzg5Mjh9.fTpcaD-6x6BDnzdWlr4PkJ1qwSSTRsi25aJ9R00mKiw"
 
 # Compose full URL for the env file
-ENV_URL="$BASE_URL/.env.$ENV"
+ENV_URL="$BASE_URL/$ENV/.env.$ENV?$TOKEN"
 
 echo "Downloading .env.$ENV..."
 curl -s -o .env "$ENV_URL"
